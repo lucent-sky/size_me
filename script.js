4daad8d66c8f02e56a62a03817c04c7de0f3d7fe
Dom Maceo <dvm004@bucknell.edu>
2025-11-17 05:14:38 -0500

    started on converting circumference to sizing

--- script.js
+++ script.js
@@ -5,6 +5,88 @@ let img = new Image();
 let clickPoints = [];
 let mmPerPixel = null;
 
+const usRingData = [
+{ size: '3', circumference: 44.0 },
+{ size: '3.5', circumference: 45.2 },
+{ size: '4', circumference: 46.5 },
+{ size: '4.5', circumference: 47.8 },
+{ size: '5', circumference: 49.0 },
+{ size: '5.5', circumference: 50.3 },
+{ size: '6', circumference: 51.5 },
+{ size: '6.5', circumference: 52.8 }, //fix later...
+{ size: '7', circumference: 54.0 },
+{ size: '7.5', circumference: 55.3 },
+{ size: '8', circumference: 56.6 },
+{ size: '8.5', circumference: 57.8 },
+{ size: '9', circumference: 59.1 },
+{ size: '9.5', circumference: 60.3 },
+{ size: '10', circumference: 61.6 },
+{ size: '10.5', circumference: 62.8 },
+{ size: '11', circumference: 64.1 },
+{ size: '11.5', circumference: 65.3 },
+{ size: '12', circumference: 66.6 },
+];
+
+const ukRingData = [
+{ circumference: 37.80, size: 'A'},
+{ circumference: 38.50, size: 'A 1/2'},
+{ circumference: 39.10, size: 'B'},
+{ circumference: 39.70, size: 'B 1/2'},
+{ circumference: 40.40, size: 'C'},
+{ circumference: 41.00, size: 'C 1/2'},
+{ circumference: 41.70, size: 'D'},
+{ circumference: 42.30, size: 'D 1/2'},
+{ circumference: 42.90, size: 'E'},
+{ circumference: 43.60, size: 'E 1/2'},
+{ circumference: 44.20, size: 'F'},
+{ circumference: 44.80, size: 'F 1/2'},
+{ circumference: 45.50, size: 'G'},
+{ circumference: 46.10, size: 'G 1/2'},
+{ circumference: 46.80, size: 'H'},
+{ circumference: 47.40, size: 'H 1/2'},
+{ circumference: 48.00, size: 'I'},
+{ circumference: 48.70, size: 'J'},
+{ circumference: 49.30, size: 'J 1/2'},
+{ circumference: 50.00, size: 'K'},
+{ circumference: 50.60, size: 'K 1/2'},
+{ circumference: 51.20, size: 'L'},
+{ circumference: 51.90, size: 'L 1/2'},
+{ circumference: 52.50, size: 'M'},
+{ circumference: 53.10, size: 'M 1/2'},
+{ circumference: 53.80, size: 'N'},
+{ circumference: 54.40, size: 'N 1/2'},
+{ circumference: 55.10, size: 'O'},
+{ circumference: 55.70, size: 'O 1/2'},
+{ circumference: 56.30, size: 'P'},
+{ circumference: 57.00, size: 'P 1/2'},
+{ circumference: 57.60, size: 'Q'},
+{ circumference: 58.30, size: 'Q 1/2'},
+{ circumference: 58.90, size: 'R'},
+{ circumference: 59.50, size: 'R 1/2'},
+{ circumference: 60.20, size: 'S'},
+{ circumference: 60.80, size: 'S 1/2'},
+{ circumference: 61.40, size: 'T'},
+{ circumference: 62.10, size: 'T 1/2'},
+{ circumference: 62.70, size: 'U'},
+{ circumference: 63.40, size: 'U 1/2'},
+{ circumference: 64.00, size: 'V'},
+{ circumference: 64.60, size: 'V 1/2'},
+{ circumference: 65.30, size: 'W'},
+{ circumference: 65.90, size: 'W 1/2'},
+{ circumference: 66.60, size: 'X'},
+{ circumference: 67.20, size: 'X 1/2'},
+{ circumference: 67.80, size: 'Y'},
+{ circumference: 68.50, size: 'Z'},
+{ circumference: 69.10, size: 'Z 1/2'},
+{ circumference: 69.70, size: 'Z 1/2'},
+{ circumference: 71.00, size: 'Z1'},
+{ circumference: 72.30, size: 'Z2'},
+{ circumference: 72.90, size: 'Z3'},
+{ circumference: 74.20, size: 'Z4'}
+]
+
+
+
 document.getElementById("imageInput").addEventListener("change", function (e) {
     let file = e.target.files[0];
     if (!file) return;
@@ -22,6 +104,7 @@ document.getElementById("imageInput").addEventListener("change", function (e) {
 });
 
 canvas.addEventListener("click", function (event) {
+    if (clickPoints.length >= 4) return;
     if (!img.src) return;
 
     let rect = canvas.getBoundingClientRect();
@@ -74,6 +157,7 @@ function computeFinger() {
 
     let fingerWidthMM = fingerPixelWidth * mmPerPixel;
     let circumference = fingerWidthMM * Math.PI;
+    
 
     showMessage(
         "Finger width: " + fingerWidthMM.toFixed(2) + " mm<br>" +
